9111315038505bb0f05a92c7ff709049c807b120
Eric Fenderbosch <eric.fenderbosch@segmint.com>
2014-12-31 15:14:07 -0500

    removed some fluff

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,33 +2,16 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-
 @Entity
-@Table(name = "test")
-@JsonInclude(Include.NON_EMPTY)
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonNaming(LowerCaseWithUnderscoresStrategy.class)
 public class Test implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@JsonSerialize(using = ToStringSerializer.class)
 	private Long id;
 	private String attribute;
 
